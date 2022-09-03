104c0b5bcb729c0bb6837860f751596649c5704e
Ahmad Haris Kurniawan <ahmadharisk@gmail.com>
2022-09-03 17:04:35 +0700

    solved arrays ds

---
+++ data_structure/arrays-DS.py
@@ -0,0 +1,13 @@
+def reverseArray(a):
+    # Write your code here
+    return a[::-1]
+
+if __name__ == '__main__':
+
+    arr_count = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    res = reverseArray(arr)
+
+    print(res)
